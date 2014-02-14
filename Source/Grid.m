534d95747c73bf9edfbd2a0fb242352583c6a035
Ben-G <Benjamin.Encz@gmail.com>
2014-02-13 19:40:30 -0800

    refactoring

--- Source/Grid.m
+++ Source/Grid.m
@@ -24,51 +24,49 @@ static const int GRID_COLUMNS = 10;
 
 - (void)onEnter
 {
-    _columnWidth = self.contentSize.width / GRID_COLUMNS;
-    _columnHeight = self.contentSize.height / GRID_ROWS;
-    float x = 0;
-    float y = 0;
-    
-    _gridArray = [NSMutableArray array];
-    
-    // initialize Creatures
-    for (int i = 0; i < GRID_ROWS; i++) {
-        _gridArray[i] = [NSMutableArray array];
-        x = 0;
-        
-        for (int j = 0; j < GRID_COLUMNS; j++) {
-            Creature *creature = [[Creature alloc] initCreature];
-            creature.anchorPoint = ccp(0, 0);
-            creature.position = ccp(x, y);
-            [self addChild:creature];
-            _gridArray[i][j] = creature;
-            
-            x+=_columnWidth;
-        }
-        
-        y += _columnHeight;
-    }
-    
+    [self setupGrid];
+  
     // accept touches on the grid
     self.userInteractionEnabled = TRUE;
 }
 
+- (void)setupGrid
+{
+  _columnWidth = self.contentSize.width / GRID_COLUMNS;
+  _columnHeight = self.contentSize.height / GRID_ROWS;
+  float x = 0;
+  float y = 0;
+  
+  _gridArray = [NSMutableArray array];
+  
+  // initialize Creatures
+  for (int i = 0; i < GRID_ROWS; i++) {
+    _gridArray[i] = [NSMutableArray array];
+    x = 0;
+    
+    for (int j = 0; j < GRID_COLUMNS; j++) {
+      Creature *creature = [[Creature alloc] initCreature];
+      creature.anchorPoint = ccp(0, 0);
+      creature.position = ccp(x, y);
+      [self addChild:creature];
+      _gridArray[i][j] = creature;
+      
+      x+=_columnWidth;
+    }
+    
+    y += _columnHeight;
+  }
+}
+
 #pragma mark - Touch Handling
 
-    - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
-
-    - (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
-
-    - (void)touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
-
-
-    - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
-    {
-        CGPoint touchLocation = [touch locationInNode:self];
-        
-        Creature *creature = [self creatureForTouchPosition:touchLocation];
-        creature.isAlive = !creature.isAlive;
-    }
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    creature.isAlive = !creature.isAlive;
+}
 
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition {
     Creature *creature = nil;
@@ -80,42 +78,55 @@ static const int GRID_COLUMNS = 10;
     return creature;
 }
 
+#pragma mark - Util function
+
+- (BOOL)indexesValid:(int)x y:(int)y
+{
+  BOOL indexesValid = TRUE;
+  indexesValid &= x > 0;
+  indexesValid &= y > 0;
+  if (indexesValid) {
+    indexesValid &= x < (int) [_gridArray count];
+    if (indexesValid) {
+      indexesValid &= y < (int) [(NSMutableArray*) _gridArray[x] count];
+    }
+  }
+  return indexesValid;
+}
+
 #pragma mark - Game Logic
+
+- (void)countNeighbours {
+  // rows
+  for (int i = 0; i < [_gridArray count]; i++) {
+    // columns
+    for (int j = 0; j < [_gridArray[i] count]; j++) {
+      Creature *currentCreature = _gridArray[i][j];
+      // reset neighbour counter
+      currentCreature.livingNeighbours = 0;
+      
+      for (int x = (i-1); x <= (i+1); x++) {
+        for (int y = (j-1); y <= (j+1); y++) {
+          
+          BOOL indexesValid;
+          indexesValid = [self indexesValid:x y:y];
+          
+          if (!((x == i) && (y == j)) && indexesValid) {
+            Creature *neighbour = _gridArray[x][y];
+            currentCreature.livingNeighbours += neighbour.isAlive;
+          }
+        }
+      }
+    }
+  }
+}
 
 - (void)evolveStep
 {
-    // rows
-    for (int i = 0; i < [_gridArray count]; i++) {
-        // columns
-        for (int j = 0; j < [_gridArray[i] count]; j++) {
-            Creature *currentCreature = _gridArray[i][j];
-            // reset neighbour counter
-            currentCreature.livingNeighbours = 0;
-            
-            for (int x = (i-1); x <= (i+1); x++) {
-                for (int y = (j-1); y <= (j+1); y++) {
-                    
-                    BOOL indexesValid = TRUE;
-                    indexesValid &= x > 0;
-                    indexesValid &= y > 0;
-                    if (indexesValid) {
-                        indexesValid &= x < (int) [_gridArray count];
-                        if (indexesValid) {
-                            indexesValid &= y < (int) [(NSMutableArray*) _gridArray[x] count];
-                        }
-                    }
-                    
-                    if (!((x == i) && (y == j)) && indexesValid) {
-                        Creature *neighbour = _gridArray[x][y];
-                        currentCreature.livingNeighbours += neighbour.isAlive;
-                    }
-                }
-            }
-        }
-    }
-    
+    [self countNeighbours];
+  
     int alive = 0;
-    
+  
     for (int i = 0; i < [_gridArray count]; i++) {
         for (int j = 0; j < [_gridArray[i] count]; j++) {
             Creature *currentCreature = _gridArray[i][j];
